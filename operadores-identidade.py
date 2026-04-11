85ef0b358d5222a1c8261c756d591bac151aba34
giancastro <gianlucca.galldorr@gmail.com>
2026-04-11 05:39:10 -0300

    adicionando operadores de associação  no arquivo de operadores de identidade.

--- operadores-identidade.py
+++ operadores-identidade.py
@@ -3,3 +3,21 @@ limite = 1000
 
 print(saldo is limite)
 print(saldo is not limite)
+
+
+
+
+
+
+
+#Operadores de associação são usados para verificar se um objeto é uma instância de uma classe ou se uma classe é uma subclasse de outra. Os principais operadores de associação são:
+# is: Retorna True se os operandos referenciam o mesmo objeto.
+# is not: Retorna True se os operandos não referenciam o mesmo objeto.
+
+
+frutas = ["maçã", "banana", "abacate", "laranja", "morango"]
+
+print("maçã" in frutas) 
+print("limão" not in frutas)
+print("banana" in frutas)
+print( "banana" not in frutas)
